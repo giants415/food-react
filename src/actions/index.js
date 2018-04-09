c515c7da92f24f7a391d79b3b91001192e481798
Andrew <andrew.vinocur@gmail.com>
2018-04-09 15:00:36 -0700

    root url defn

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const FETCH_RECIPES = 'fetch_recipes';
 
+const ROOT_URL = 'http://food2fork.com/api/search';
+
 export function fetchRecipes() {
   const request = axios.get()
 
