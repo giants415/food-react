c9a50591352f56c55f6f681765867dcd9ceabb3f
Andrew <andrew.vinocur@gmail.com>
2018-04-09 13:46:49 -0700

    adds imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
 ReactDOM.render(
   <div>
-    
+
   </div>
   , document.getElementById('root'));
 registerServiceWorker();
