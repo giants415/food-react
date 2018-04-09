74bbdaa7cbbf9d8b13cb069a0a7aa787dfd6ceab
Andrew <andrew.vinocur@gmail.com>
2018-04-09 14:23:26 -0700

    boiler plate and test text for first component

---
+++ src/components/recipe_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RecipeIndex extends Component {
+
+  render() {
+    return(
+      <div>
+        <h2>Recipe Index Page!</h2>
+      </div>
+    );
+  }
+}
+
+export default RecipeIndex;
