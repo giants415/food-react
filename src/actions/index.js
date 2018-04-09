723dc4a2f01c54137078061af25cba84cd8378d5
Andrew <andrew.vinocur@gmail.com>
2018-04-09 15:47:52 -0700

    sets up base get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
+import KEY from '../keys';
 
 export const FETCH_RECIPES = 'fetch_recipes';
 
 const ROOT_URL = 'http://food2fork.com/api/search';
+const API_KEY = KEY;
 
 export function fetchRecipes() {
-  const request = axios.get()
+  const request = axios.get(`${ROOT_URL}?key=${API_KEY}`);
 
   return {
-    type: FETCH_RECIPES
+    type: FETCH_RECIPES,
+    payload: request
   };
 }
