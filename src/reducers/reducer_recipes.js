e1e670a7d84888559d80e85888fd8874c7d846c1
Andrew <andrew.vinocur@gmail.com>
2018-04-09 16:00:57 -0700

    in progress commits

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,8 +1,9 @@
 import { FETCH_RECIPES } from '../actions';
 
-export default function(state, action) {
+export default function(state = {}, action) {
   switch (action.type) {
-
+    case FETCH_RECIPES:
+      return // SOMETHING
     default:
       return state;
   }
