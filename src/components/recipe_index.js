55f3060811cdfbd24375f56cf3989249c49a6dc3
Andrew <andrew.vinocur@gmail.com>
2018-04-10 15:39:57 -0700

    works for showing single recipe

--- src/components/recipe_index.js
+++ src/components/recipe_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchRecipes } from '../actions';
@@ -7,10 +8,24 @@ class RecipeIndex extends Component {
     this.props.fetchRecipes();
   }
 
+  renderRandomRecipe() {
+    return _.map(this.props.recipes, recipe => {
+      return (
+        <p key={recipe.id}>
+          {recipe.strMeal}
+        </p>
+      );
+    })
+  }
+
   render() {
     return(
       <div>
         <h2>Recipe Index Page!</h2>
+        <div>
+          <h4>Random Recipe of the Moment</h4>
+          <p>{this.renderRandomRecipe()}</p>
+        </div>
       </div>
     );
   }
