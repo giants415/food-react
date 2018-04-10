486299fa05403a930648ae4f60c9e3337bdc4815
Andrew <andrew.vinocur@gmail.com>
2018-04-10 15:35:36 -0700

    correctly takes idMeal and uses it as key on object

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import { FETCH_RECIPES } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_RECIPES:
-      return action.payload.data;
+      return _.mapKeys(action.payload.data.meals, 'idMeal');
     default:
       return state;
   }
