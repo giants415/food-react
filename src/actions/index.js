f26f713d6bc0aab7c61f63f52b6cd64ca414c748
Andrew <andrew.vinocur@gmail.com>
2018-04-10 09:59:59 -0700

    adds comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,9 @@ const ROOT_URL = 'http://food2fork.com/api/search';
 const API_KEY = KEY;
 
 export function fetchRecipes() {
+  // this will return the top 30 recipes on food2fork
+  // this will serve as a landing page option since there will
+  //   be no search at launch
   const request = axios.get(`${ROOT_URL}?key=${API_KEY}`);
 
   return {
