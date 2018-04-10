6e15d3175b79a1bfb7b3666840dc5a888a47bf16
Andrew <andrew.vinocur@gmail.com>
2018-04-10 16:40:57 -0700

    adds a default height

--- src/components/recipe_index.js
+++ src/components/recipe_index.js
@@ -13,7 +13,7 @@ class RecipeIndex extends Component {
       return (
         <div key={recipe.idMeal}>
           <h5><a href={recipe.strSource}>{recipe.strMeal}</a></h5>
-          <img src={recipe.strMealThumb} alt="random recipe display"/>
+          <img height="300px" src={recipe.strMealThumb} alt="random recipe display"/>
         </div>
       );
     })
