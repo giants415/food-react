0e7a5c125f8599f322e7e7a0ad7a042dd11b36cc
Andrew <andrew.vinocur@gmail.com>
2018-04-10 13:14:22 -0700

    food2fork doesnt work, switching to mealdb api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
-import KEY from '../keys';
 
 export const FETCH_RECIPES = 'fetch_recipes';
 
-const ROOT_URL = 'http://food2fork.com/api/search';
-const API_KEY = KEY;
+const ROOT_URL = 'https://www.themealdb.com/api/json/v1/1';
 
 export function fetchRecipes() {
   // this will return the top 30 recipes on food2fork
   // this will serve as a landing page option since there will
   //   be no search at launch
-  const request = axios.get(`${ROOT_URL}?key=${API_KEY}`);
-  console.log(request);
+  const request = axios.get(`${ROOT_URL}/random.php`);
 
   return {
     type: FETCH_RECIPES,
