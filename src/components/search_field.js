c91ac8be85fba9ca1b5363dc8303e17de12cf2b7
Andrew <andrew.vinocur@gmail.com>
2018-04-10 22:36:56 -0700

    boilers plate for render search and adds label to field

--- src/components/search_field.js
+++ src/components/search_field.js
@@ -3,10 +3,20 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
 class SearchField extends Component {
+  renderSearchField(field) {
+    return (
+      <div>
+        <label>{field.label}</label>
+      </div>
+    )
+  }
+
+
   render() {
     return (
       <form>
         <Field
+          label="Search By A Main Ingredient"
           name="search"
           component={this.renderSearchField}
         />
