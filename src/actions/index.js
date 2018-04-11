a028d4603259ed4af195a44653f0a06743338883
Andrew <andrew.vinocur@gmail.com>
2018-04-10 22:23:02 -0700

    adds fetch main ingredient action boiler plate

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_RANDOM = 'fetch_random';
+export const FETCH_MAIN_INGREDIENT = 'fetch_main_ingredient';
 
 const ROOT_URL = 'https://www.themealdb.com/api/json/v1/1';
 
@@ -14,4 +15,13 @@ export function fetchRandom() {
     type: FETCH_RANDOM,
     payload: request
   };
+}
+
+export function fetchMainIngredient(mainIngredient) {
+  function(mainIngredient) {
+    console.log(mainIngredient);
+    // need to build helper function to format mainIngredient according to API
+  }
+
+  const request = axios.get(`${ROOT_URL}/filter.php?i=${mainIngredient}`);
 }
