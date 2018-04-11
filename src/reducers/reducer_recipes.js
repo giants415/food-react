ac31ecc5e1533bd9e0f383d8d578c6a95bd5694e
Andrew <andrew.vinocur@gmail.com>
2018-04-10 22:25:15 -0700

    adds action to reducer

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
-import { FETCH_RANDOM } from '../actions';
+import { FETCH_RANDOM, FETCH_MAIN_INGREDIENT } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_RANDOM:
+      return _.mapKeys(action.payload.data.meals, 'idMeal');
+    case FETCH_MAIN_INGREDIENT:
       return _.mapKeys(action.payload.data.meals, 'idMeal');
     default:
       return state;
