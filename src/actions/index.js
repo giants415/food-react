e042a81dfc58b20b4cf7fdf70ceeec7131fc284b
Andrew <andrew.vinocur@gmail.com>
2018-04-11 15:59:29 -0700

    removes comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,9 +6,6 @@ export const FETCH_MAIN_INGREDIENT = 'fetch_main_ingredient';
 const ROOT_URL = 'https://www.themealdb.com/api/json/v1/1';
 
 export function fetchRandom() {
-  // this will return the top 30 recipes on food2fork
-  // this will serve as a landing page option since there will
-  //   be no search at launch
   const request = axios.get(`${ROOT_URL}/random.php`);
 
   return {
