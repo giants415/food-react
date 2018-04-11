84ff9cfae0418909efb2d6a985260c4dac27be2e
Andrew <andrew.vinocur@gmail.com>
2018-04-10 22:14:12 -0700

    boiler plate

---
+++ src/components/main_ingredient_show.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class MainIngredientShow extends Componentc {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchMainIngredient })(MainIngredientShow);
