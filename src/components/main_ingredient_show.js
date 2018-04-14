96b960f5777dbb57ead2c0587edc5c1a3c3dcfac
Andrew <andrew.vinocur@gmail.com>
2018-04-14 16:13:03 -0700

    boiler plate for render recipes

--- src/components/main_ingredient_show.js
+++ src/components/main_ingredient_show.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class MainIngredientShow extends Component {
+  renderRecipes() {
+    return _.map(this.props.recipes, recipe => {
+      return (
+        <div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
