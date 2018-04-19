02863b4f86a1a6e6cf2475b5d2fcaeb304d2c02f
Andrew <andrew.vinocur@gmail.com>
2018-04-19 10:10:58 -0700

    working display of search results from axios request

--- src/components/main_ingredient_show.js
+++ src/components/main_ingredient_show.js
@@ -10,16 +10,21 @@ class MainIngredientShow extends Component {
     return _.map(this.props.recipes, recipe => {
       return (
         <div>
+          <p>{recipe.strMeal}</p>
         </div>
       )
     })
   }
 
   render() {
+    console.log(this.props.recipes);
     return (
       <div>
         <h2>Main Ingredient Search Page</h2>
         <SearchField />
+        <div>
+          {this.renderRecipes()}
+        </div>
       </div>
     );
   }
