baa5dcc8a4123b9fdd6a0799b2ca8835d9f89584
Andrew <andrew.vinocur@gmail.com>
2018-04-18 17:49:06 -0700

    imports link from redux form

--- src/components/recipe_index.js
+++ src/components/recipe_index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchRandom } from '../actions';
 
