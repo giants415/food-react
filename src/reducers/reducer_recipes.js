c15a14607d75ad379b0ef8b7a1115ef7638161b0
Andrew <andrew.vinocur@gmail.com>
2018-04-19 18:28:35 -0700

    adds fetch recipe to reducer

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_RANDOM, FETCH_MAIN_INGREDIENT } from '../actions';
+import { FETCH_RANDOM, FETCH_MAIN_INGREDIENT, FETCH_RECIPE } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
@@ -8,6 +8,8 @@ export default function(state = {}, action) {
     case FETCH_MAIN_INGREDIENT:
       console.log(_.mapKeys(action.payload.data.meals, 'idMeal'));
       return _.mapKeys(action.payload.data.meals, 'idMeal');
+    case FETCH_RECIPE:
+      console.log(action.payload.data); 
     default:
       return state;
   }
