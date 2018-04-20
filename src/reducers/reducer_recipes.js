8e9596989b819285be81a36dd5d7846f6501daa6
Andrew <andrew.vinocur@gmail.com>
2018-04-20 14:28:10 -0700

    removes a console log

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -6,10 +6,9 @@ export default function(state = {}, action) {
     case FETCH_RANDOM:
       return _.mapKeys(action.payload.data.meals, 'idMeal');
     case FETCH_MAIN_INGREDIENT:
-      console.log(_.mapKeys(action.payload.data.meals, 'idMeal'));
       return _.mapKeys(action.payload.data.meals, 'idMeal');
     case FETCH_RECIPE:
-      console.log(action.payload.data); 
+      console.log(action.payload.data);
     default:
       return state;
   }
