09f0b89f9ae5ba1279f3257d1892fd5734939f77
Andrew <andrew.vinocur@gmail.com>
2018-04-20 16:06:05 -0700

    adds structure of recipe to be displayed

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -3,8 +3,21 @@ import { connect } from 'react-redux';
 
 class Recipe extends Component {
   render() {
-    return (
+    const {
+      name,
+      thumbnail
+    } = this.props;
 
+    return (
+      <div className="card" style={cardStyle}>
+        <img className="card-img-top" src={thumbnail} alt="Recipe" />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <button className="btn btn-outline-primary" onClick={this.onViewDetails}>
+            View Recipe Details
+          </button>
+        </div>
+      </div>
     );
   }
 }
