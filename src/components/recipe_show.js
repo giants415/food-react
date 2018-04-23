04e828b7e20b52fd2f32d42fc2287c3deaf64941
Andrew <andrew.vinocur@gmail.com>
2018-04-23 11:49:31 -0700

    boiler palte

--- src/components/recipe_show.js
+++ src/components/recipe_show.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class RecipeShow extends Component {
   render() {
     return(
-
+      <div>
+        
+      </div>
     );
   }
 }
 
-export default;
+export default RecipeShow;
