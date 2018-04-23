0939ccea0e366e099e5eb291ecac36c0bb6fd0a3
Andrew <andrew.vinocur@gmail.com>
2018-04-23 15:46:39 -0700

    builds out recipe show component

--- src/components/recipe_show.js
+++ src/components/recipe_show.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchRecipe } from '../actions';
 
 class RecipeShow extends Component {
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    const id = this.props.match.params.id;
+    this.props.fetchRecipe(id);
+    console.log('props', this.props);
+  }
+
   render() {
+    const { recipe } = this.props;
+
+    if (!recipe) {
+      return <div>Loading...</div>;
+    }
+
     return(
       <div>
-        
+        <h2>Selected Recipe:</h2>
+        <h3>{recipe.strMeal}</h3>
       </div>
     );
   }
 }
 
-export default RecipeShow;
+function mapStateToProps(state) {
+  return { recipe: state.recipes };
+}
+
+export default connect(mapStateToProps, { fetchRecipe })(RecipeShow);
