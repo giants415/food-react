beee050f642795cd5ab4355b12f72ebd7b8ca9e9
Andrew <andrew.vinocur@gmail.com>
2018-05-01 17:24:27 -0700

    imports new random reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import RecipesReducer from './reducer_recipes';
+import RandomReducer from './reducer_random';
 
 const rootReducer = combineReducers({
+  randomRecipe: RandomReducer,
   recipes: RecipesReducer,
   form: formReducer
 });
