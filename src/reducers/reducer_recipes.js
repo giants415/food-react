42f67ad8294d6366d5efe5e2f9876a694523d80e
Andrew <andrew.vinocur@gmail.com>
2018-05-01 17:24:20 -0700

    removes fetch random

--- src/reducers/reducer_recipes.js
+++ src/reducers/reducer_recipes.js
@@ -1,10 +1,8 @@
 import _ from 'lodash';
-import { FETCH_RANDOM, FETCH_MAIN_INGREDIENT, FETCH_RECIPE } from '../actions';
+import { FETCH_MAIN_INGREDIENT, FETCH_RECIPE } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
-    case FETCH_RANDOM:
-      return _.mapKeys(action.payload.data.meals, 'idMeal');
     case FETCH_MAIN_INGREDIENT:
       return _.mapKeys(action.payload.data.meals, 'idMeal');
     case FETCH_RECIPE:
